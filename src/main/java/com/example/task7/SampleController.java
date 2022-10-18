0eb7bbc20e458d69f4d841e847e626afea29f0d9
Oonishi228 <oasttsv8@gmail.com>
2022-10-18 21:58:37 +0900

    GET/POST/PATCH/DELETEのリクエストを扱えるControllerを実装しました。

---
+++ src/main/java/com/example/task7/SampleController.java
@@ -0,0 +1,36 @@
+package com.example.task7;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class SampleController {
+    @GetMapping("/names")
+    public List<String> getNames() {
+        return List.of("koyama", "tanaka");
+    }
+
+    @PostMapping("/names")
+    public ResponseEntity<String> create(@RequestBody @Validated CreateForm form) {
+        URI url = UriComponentsBuilder.fromUriString("http://localhost:8080").path("/names/id")
+                .build()
+                .toUri();
+        return ResponseEntity.created(url).body("name successfully created");
+    }
+
+    @PatchMapping("/names/{id}")
+    public ResponseEntity<Map<String, String>> update(@PathVariable("id") int id, @RequestBody UpdateForm form) {
+        return ResponseEntity.ok(Map.of("message", "name successfully updated"));
+    }
+
+    @DeleteMapping("/names/{id}")
+    public ResponseEntity<Map<String, String>> delete(@PathVariable("id") int id) {
+        return ResponseEntity.ok(Map.of("message", "name successfully deleted"));
+    }
+}
