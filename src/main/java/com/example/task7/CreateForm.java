4a87dacbd01722740f5f56e869d28767d3e2136b
Oonishi228 <oasttsv8@gmail.com>
2022-10-23 15:22:22 +0900

    誕生日を生年月日の形式で受け付けるバリデーションを追加。誕生日の更新を受け付けるようUpdateForm.javaに追記しました。

--- src/main/java/com/example/task7/CreateForm.java
+++ src/main/java/com/example/task7/CreateForm.java
@@ -3,12 +3,16 @@ package com.example.task7;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 public class CreateForm {
     @NotBlank
     @Length(max = 20)
     private String name;
-    private final String birthday;
+
+    @NotBlank
+    @Pattern(regexp = "^19|20[0-9]{2}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$")//YYYY-mm-ddの形式。YYYYは1900〜2099でないとエラー
+    private String birthday;
 
     public CreateForm(String name, String birthday) {
         this.name = name;
@@ -27,5 +31,9 @@ public class CreateForm {
     public void setName(String name) {
         this.name = name;
     }
-    
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
 }
--- src/main/java/com/example/task7/UpdateForm.java
+++ src/main/java/com/example/task7/UpdateForm.java
@@ -3,17 +3,31 @@ package com.example.task7;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 public class UpdateForm {
     @NotBlank
     @Length(max = 20)
     private String name;
 
+    @NotBlank
+    @Pattern(regexp = "^19|20[0-9]{2}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$")//YYYY-mm-dd
+    private String birthday;
+
     public String getName() {
         return name;
+    }
+
+    public String getBirthday() {
+        return birthday;
     }
 
     public void setName(String name) {
         this.name = name;
     }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
 }
